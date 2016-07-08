52e68b81ccd060ffbd94cb75c5227a195d0f7881
Peter Breen <pbreen@gmail.com>
2016-07-08 11:46:10 -0700

    no progress save commit

--- js/generateTable.js
+++ js/generateTable.js
@@ -1,15 +1,18 @@
-$.getJSON({
-  type: 'GET',
-  url: '../data/ciderlist.json',
-  success: function (data) {
-    var tableRow;
-    for (var i = 0; i < data.length; i++) {
-      tableRow = $('<tr />');
-      tableRow.append('<td>' + data[i].Brand + '</td>');
-      tableRow.append('<td>' + data[i].Cider + '</td>');
-      tableRow.append('<td>' + data[i].Rating + '</td>');
-      tableRow.append('<td>' + data[i].Notes + '</td>');
-      $('#cider-table').append(tableRow);
+$(document).ready(function(){
+  $.getJSON({
+    type: 'GET',
+    url: 'data/ciderlist.json',
+    success: function (data) {
+      console.log('JSON get success');
+      var tableRow;
+      for (var i = 0; i < data.length; i++) {
+        tableRow = $('<tr />');
+        tableRow.append('<td>' + data[i].Brand + '</td>');
+        tableRow.append('<td>' + data[i].Cider + '</td>');
+        tableRow.append('<td>' + data[i].Rating + '</td>');
+        tableRow.append('<td>' + data[i].Notes + '</td>');
+        $('#cider-table').append(tableRow);
+      }
     }
-  }
+  });
 });
