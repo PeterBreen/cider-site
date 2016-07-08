61897860ad1dfbc1382aac326ffd4e9ed29bf4df
Peter Breen <pbreen@gmail.com>
2016-07-08 10:49:56 -0700

    changed to jquery ajax call for json, jquery table append

--- js/generateTable.js
+++ js/generateTable.js
@@ -1,26 +1,15 @@
-$(document).ready(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'data/ciderlist.json',
-    dataType: 'json',
-    success: function(xmlData) {
-      $('record', xmlData).each(function(){
-        var brand = $(this).find('Brand').text();
-        var cider = $(this).find('Cider').text();
-        var rating = $(this).find('Rating').text();
-        var notes = $(this).find('Notes').text();
-        var output = '<tr>';
-        output += '<td class="brand">' + brand + '</td>';
-        output += '<td class="cider">' + cider + '</td>';
-        output += '<td class="rating">' + rating + '</td>';
-        output += '<td class="notes">' + notes + '</td>';
-        output += '</tr>';
-        $('#cider-table tbody').append(output);
-        var options = {
-          valueNames: [ 'brand', 'cider', 'rating', 'notes' ]
-        };
-        var userList = new List('listjs', options);
-      });
+$.getJSON({
+  type: 'GET',
+  url: 'data/ciderlist.json',
+  success: function (data) {
+    var tableRow;
+    for (var i = 0; i < data.length; i++) {
+      tableRow = $('<tr />');
+      tableRow.append('<td>' + data[i].Brand + '</td>');
+      tableRow.append('<td>' + data[i].Cider + '</td>');
+      tableRow.append('<td>' + data[i].Rating + '</td>');
+      tableRow.append('<td>' + data[i].Notes + '</td>');
+      $('#cider-table').append(tr);
     }
-  });
+  }
 });
