a99ae0bc2d8d35257df55ca789bdd86011319c65
Peter Breen <pbreen@gmail.com>
2016-07-12 11:49:53 -0700

    getJSON works properly, need to attach to template and render

--- js/generateTable.js
+++ js/generateTable.js
@@ -1,42 +1,12 @@
-//uses code and logic from https://gist.github.com/chrislkeller/3230081
 
-//begin main function; needs DOM ready state
-$(document).ready(function(){
-  retriveData();
-});
-
-// grab data
-function retriveData() {
-  var dataSource = '../data/ciderlist.json';
-  $.getJSON(dataSource, renderDataVisualsTemplate);
+// render ciderlist from JSON data
+var renderFromJSON = function() {
+  var returnedData = $.getJSON('/data/ciderlist.json');
+  console.log(returnedData);
+  return returnedData;
 };
 
-// create projects content template
-function renderDataVisualsTemplate(data){
-  getTemplateAjax('dataDetailsTemplate.handlebars', function(template) {
-    handlebarsDebugHelper(); //this function should be removed after debug is complete
-    $('#cider-table-body').html(template(data));
-  });
-};
-
-// render handlebars templates via ajax
-function getTemplateAjax(path, callback) {
-  var source, template;
-  $.ajax({
-    url: path,
-    success: function (data) {
-      source = data;
-      template = Handlebars.compile(source);
-      if (callback) callback(template);
-    }
-  });
-}
-//end
-
-// add handlebars debugger
-function handlebarsDebugHelper(){
-  Handlebars.registerHelper('debug', function(optionalValue) {
-    console.log('Current Context: ' + this);
-  });
-};
-// end
+var source = $('#cider-table-body-template').html();
+var template = Handlebars.compile(source);
+var context = renderFromJSON();
+var html = template(context);
